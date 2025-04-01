471dc9be40d55ac55985481057f8298e7814a7db
Eal <alexander-zanker@web.de>
2025-04-01 20:17:10 +0200

    Chore: remove unnecessary attribute

--- src/main/java/de/computacenter/kfzkonfigurator/carconfigurations/model/sonderausstattungen/Sonderausstattung.java
+++ src/main/java/de/computacenter/kfzkonfigurator/carconfigurations/model/sonderausstattungen/Sonderausstattung.java
@@ -1,21 +1,12 @@
 package de.computacenter.kfzkonfigurator.carconfigurations.model.sonderausstattungen;
 
 public abstract class Sonderausstattung {
-    private Long id;
     private String bezeichnung;
 
     public Sonderausstattung() {}
 
     public Sonderausstattung(String bezeichnung) {
         this.bezeichnung = bezeichnung;
-    }
-
-    public Long getId() {
-        return id;
-    }
-    
-    public void setId(Long id){
-        this.id = id;
     }
 
     public String getBezeichnung() {
